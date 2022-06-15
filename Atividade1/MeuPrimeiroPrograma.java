4f81115fc67f923d465928a0b2c823e1318dcf14
Isabela Cristina <isabelacristina20005@gmail.com>
2022-06-15 20:12:41 -0300

    Atividades do 1° Trimestre

--- Atividade1/MeuPrimeiroPrograma.java
+++ Atividade1/MeuPrimeiroPrograma.java
@@ -1,4 +1,4 @@
-public class MeuPrimeiroPrograma{
+public class MeuPrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println( "– Laboratório de Programação 2 – LP2\n – 2 ano de Informática\n – Seja bem vindo!\n – Vamos aprender Java." );
     }
